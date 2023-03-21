d70683974533976c90bf910b98bc4da4b1ae0bc5
jihojun1 <shj9288@naver.com>
2023-03-21 17:31:28 +0900

    hello-react폴더

---
+++ hello-react/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import LifeCycleSample from './LifeCycleSample';
+
+class App extends Component {
+  render() {
+    return <LifeCycleSample />;
+  }
+}
+export default App;
