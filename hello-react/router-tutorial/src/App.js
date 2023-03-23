9dc1980c5f97bd9ea341a0d79e8f90962bc35a98
jihojun1 <shj9288@naver.com>
2023-03-23 18:01:57 +0900

    세번째 커밋

--- hello-react/router-tutorial/src/App.js
+++ hello-react/router-tutorial/src/App.js
@@ -1,14 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
 import About from './pages/About';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Article from './pages/Article';
+import Articles from './pages/Articles';
+import NotFound from './pages/NotFound';
+import Login from './pages/Login';
+import MyPage from './pages/MyPage';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
